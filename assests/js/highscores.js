70500475798d81ca73f0df74231e5388edcd5a3d
RyanMAWalker <rmawalk@hotmail.com>
2021-12-22 12:49:39 +0000

    Add quit btn, add id to highscores btn

--- assests/js/highscores.js
+++ assests/js/highscores.js
@@ -1,7 +1,9 @@
-/*jshint esversion: 6 */
+/* jshint esversion 6 */
 const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+
+//interacts with high scores on the homepage. 
 highScoresList.innerHTML = highScores.map( score => {
     return (`<li class="high-score">${score.name} - ${score.score}</li>`);
 }).join("");
