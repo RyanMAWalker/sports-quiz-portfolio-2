184493a75a22564f07ad2c9f59ae36d936008234
RyanMAWalker <rmawalk@hotmail.com>
2021-12-22 15:10:46 +0000

    fix file path of highscores storage

--- assests/js/submit.js
+++ assests/js/submit.js
@@ -39,7 +39,7 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('index.html');
 };
 
 // quit button 
