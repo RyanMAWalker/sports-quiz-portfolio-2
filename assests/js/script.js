1a4e683dd3c216ea6f05850d9a582b70b2c01651
RyanMAWalker <rmawalk@hotmail.com>
2021-12-22 04:51:15 +0000

    Convert answer-text to an array

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMcontentLoaded", function(){
     startQuiz();
 })
 
+//List of references
+
+const question = document.getElementById("question");
+
+//convert the string of answers to an array
+const answers = Array.from(document.getElementsByClassName("answer-text"));
+
+console.log(answers);
 
 function startQuiz () {
 
