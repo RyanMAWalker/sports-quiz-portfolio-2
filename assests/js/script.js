793b56e9a2365bf2ec40372ff55129be931284da
RyanMAWalker <rmawalk@hotmail.com>
2021-12-22 05:39:45 +0000

    Add random question display function

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,13 +5,11 @@ document.addEventListener("DOMcontentLoaded", function () {
 
 //List of references
 
+//Get question element from the DOM
 const question = document.getElementById("question");
 
 //convert the string of answers to an array
-const answers = Array.from(document.getElementsByClassName("answer-text"));
-console.log(answers);
-
-function startQuiz() {}
+const answersOptions = Array.from(document.getElementsByClassName("answer-text"));
 
 //To start quiz function I need questions to define in the form of an array
 
@@ -57,3 +55,52 @@ let questions = [
         answer: 3
     },
 ]
+
+//Define variables
+
+let currentQuestion = {}; 
+let acceptingAnswers = false;
+let avaliableQuestions = [];
+//Incrimental vairables
+let score = 0;
+let questionCounter = 0;
+
+
+//Game constants
+const maxQuestions = 5;
+const correctAnswer = 10;
+
+//Code line taken from Slack and Discord see assets/images/discord and
+//assets/images/slack
+/**
+ * The Main game function, to start the quiz.
+ */
+function startQuiz () {
+    score = 0;
+    questionCounter = 0;
+    avaliableQuestions = [ ... questions]
+    console.log(avaliableQuestions)
+    getNewQuestion();
+}
+
+const getNewQuestion = function () {
+
+// Increases counter from 0 by 1
+    questionCounter++; 
+
+//Generates a random number from the avaliable questions array defined in the startQuiz function.
+//.length specifies the number of questions avaliable from [...questions]
+    const questionIndex = Math.floor(Math.random() * avaliableQuestions.length);
+    currentQuestion = avaliableQuestions[questionIndex];
+
+// inputs the text of the "pulled" question
+    question.innerText = currentQuestion.question; 
+
+//Removes the question just used
+    avaliableQuestions.splice(questionIndex, 1); 
+    };
+
+startQuiz();
+
+
+
