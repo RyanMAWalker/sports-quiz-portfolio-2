7f3aa9f3002f09313aa8c27cb9c07c46484bcdc2
RyanMAWalker <rmawalk@hotmail.com>
2021-12-22 11:24:46 +0000

    Remove reutrn function from submit.js, unecessary

--- assests/js/submit.js
+++ assests/js/submit.js
@@ -1,40 +1,45 @@
+/*jshint esversion: 6 */
+
 // Code base for highscores taken from youtube video https://www.youtube.com/watch?v=f4fB9Xg2JEY
 
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+//Converts high scores recieved from local storage and converts into an array or if returns null
+//Initialises an empty array is produced
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const maxHighScores = 50; 
 
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 //Set to disabled unless the input username section is populated.
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
-})
+});
 
 // Prevents user from clicking save
 saveHighScore = e => {
     e.preventDefault();
 
+    //
     const score = {
         score: mostRecentScore,
         name: username.value
-    }
+    };
 
+    //Adds score to list
     highScores.push(score);
 
-    highScores.sort((a,b) => {
-        return b.score - a.score;
-    })
+    //Sorts list
+    highScores.sort((a,b) =>  b.score - a.score);
 
+    //Cuts off top 5 scores
     highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
+    localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('/');
 };
 
